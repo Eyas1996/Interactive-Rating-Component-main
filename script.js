99d369b3e9836e8c8c60a529e28faaf0e15c665e
Eyas1996 <eyas.hm7@gmail.com>
2023-01-23 17:41:41 +0100

    Upload Files

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let btn = document.getElementById("btn");
-let numbers = document.querySelectorAll(".numbers span");
+let numbers = document.querySelectorAll(".numbers button");
 let main = document.getElementById("main-card");
 let result = document.querySelector(".result");
 
 let ratingValue = "";
 numbers.forEach(function (ele) {
-  ele.onclick = () => {
-    ele.style.backgroundColor = "hsl(25deg 97% 53%)";
-    ele.style.color = "hsl(0, 0%, 100%)";
+  ele.onfocus = () => {
+    ratingValue = ele.textContent;
     btn.style.backgroundColor = "hsl(0, 0%, 100%)";
     btn.style.color = "hsl(25deg 97% 53%)";
-
-    ratingValue = ele.textContent;
-
-    btn.onclick = function () {
-      if (ratingValue !== "") {
-        result.textContent = `You selected ${ratingValue} out of 5`;
-        console.log(ratingValue);
-      }
+    ele.onblur = () => {
+      btn.style.backgroundColor = "hsl(25deg 97% 53%)";
+      btn.style.color = "hsl(0, 0%, 100%)";
+    }
+  }
+  btn.onclick = function () {
+    if (ratingValue !== "") {
+      result.textContent = `You selected ${ratingValue} out of 5`;
       main.classList.add("z-index");
-    };
+    } else {
+      alert("Please choose rating!!");
+    }
   };
+
 });
